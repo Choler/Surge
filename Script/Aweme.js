ff0a25b77c68a21a2b2ca5e36ecd06521471f832
Choler <43330069+Choler@users.noreply.github.com>
2019-06-21 18:42:25 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -13,25 +13,6 @@ keyword.forEach(function(k) {
 var obj = JSON.parse(result);
 
 obj.aweme_list.forEach((element, index) => {
-  if (element.simple_promotions) {
-    delete obj.aweme_list[index].simple_promotions;
-  }
-});
-
-obj.aweme_list.forEach((element, index) => {
-  if (element.interaction_stickers !== null) {
-    obj.aweme_list[index].interaction_stickers = null;
-  }
-});
-
-obj.aweme_list.forEach((element, index) => {
-  if (element.prevent_download === true) {
-    obj.aweme_list[index].prevent_download = false;
-    obj.aweme_list[index].status.reviewed = 1;
-  }
-});
-
-obj.aweme_list.forEach((element, index) => {
   if (element.is_ads === true) {
     obj.aweme_list.splice(index, 1);
   }
