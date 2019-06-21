7c4bd1faafcfe2fd73fbb483aeee416c713517b5
Choler <43330069+Choler@users.noreply.github.com>
2019-06-21 22:58:26 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,5 +1,5 @@
 function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(find, 'g'), replace);
+  return str.replace(new RegExp(find, "g"), replace);
 }
 
 var keyword = ['watermark=1'];
@@ -7,17 +7,20 @@ var keyword = ['watermark=1'];
 var result = $response.body;
 
 keyword.forEach(function(k) {
-  result = replaceAll(result, k, 'watermark=0');
+  result = replaceAll(result, k, "watermark=0");
 });
 
-var obj = JSON.parse(result);
-
-obj.aweme_list.forEach((element, index) => {
-  if (element.is_ads === true) {
-    obj.aweme_list.splice(index, 1);
-  }
-});
-
-body = JSON.stringify(obj);
-
-$done({body});
+if ($request.url.indexOf('post') != -1) {
+  var obj = JSON.parse(result);
+  body = JSON.stringify(obj);
+  $done({body});
+} else if ($request.url.indexOf('feed') != -1) {
+  var obj = JSON.parse(result);
+  obj.aweme_list.forEach((element, index) => {
+    if (element.is_ads === true) {
+      obj.aweme_list.splice(index, 1);
+    }
+  });
+  body = JSON.stringify(obj);
+  $done({body});
+}
