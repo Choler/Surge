3c8080b396279ad1ffbabbab3847de6479c17159
Choler <43330069+Choler@users.noreply.github.com>
2019-06-22 12:10:33 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -10,17 +10,17 @@ keyword.forEach(function(k) {
   result = replaceAll(result, k, "watermark=0");
 });
 
+var obj = JSON.parse(result);
+
 if ($request.url.indexOf('post') != -1) {
-  var obj = JSON.parse(result);
-  body = JSON.stringify(obj);
-  $done({body});
+  body = JSON.stringify(obj)
 } else if ($request.url.indexOf('feed') != -1) {
-  var obj = JSON.parse(result);
   obj.aweme_list.forEach((element, index) => {
     if (element.is_ads === true) {
       obj.aweme_list.splice(index, 1);
+      body = JSON.stringify(obj)
     }
   });
-  body = JSON.stringify(obj);
-  $done({body});
 }
+
+$done({body});
