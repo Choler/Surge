776f59dda3c38dabad8b5ab62c9d3dc7de39ff8a
Choler <43330069+Choler@users.noreply.github.com>
2019-06-24 06:38:36 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,24 +1,25 @@
+let result = $response.body;
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(find, 'g'), replace);
 }
-
 var keyword = ['watermark=1'];
-
-var result = $response.body;
-
 keyword.forEach(function(k) {
   result = replaceAll(result, k, 'watermark=0');
 });
-
 body = JSON.parse(result);
-body['aweme_list'].forEach((element, index) => {
-  if (element.is_ads === true) {
-    body['aweme_list'].splice(index, 1);
+body.aweme_list.forEach((element, index)=>{
+  if(element.hasOwnProperty('raw_ad_data')){      
+  body.aweme_list.splice(index, 1);
   }
 });
-body['aweme_list'].forEach((element, index) => {
-  if (element.simple_promotions) {
-    delete body['aweme_list'][index].simple_promotions;
+body.aweme_list.forEach((element, index)=>{
+  if(element.hasOwnProperty('simple_promotions')){      
+  delete body.aweme_list[index].simple_promotions;
+  }
+});
+body.aweme_list.forEach((element, index) => {
+  if (element.interaction_stickers !== null) {
+    body.aweme_list[index].interaction_stickers = null;
   }
 });
 body['aweme_list'].forEach((element, index) => {
@@ -27,6 +28,5 @@ body['aweme_list'].forEach((element, index) => {
     body['aweme_list'][index].prevent_download = false;
   }
 });
-body = JSON.stringify(body)
-
-$done({body})
+body = JSON.stringify(body);
+$done({body});
