2e7b2b2b5dc8cd3a94babfba9fc9708a0cc5515a
Choler <43330069+Choler@users.noreply.github.com>
2019-06-24 22:09:09 +0800

    Add files via upload

--- Script/WeChat.js
+++ Script/WeChat.js
@@ -2,3 +2,11 @@ body = JSON.parse($response.body);
 body.advertisement_info = [];
 body = JSON.stringify(body);
 $done({body});
+
+/**********************************************************
+[Script]
+http-response ^https:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad script-path=https://raw.githubusercontent.com/Choler/Surge/master/Script/WeChat.js,requires-body=true,max-size=131072
+
+[MITM]
+hostname = mp.weixin.qq.com
+**********************************************************/
