446d16c75a1fb269ead5edcf5db659e70e6f0493
Choler <43330069+Choler@users.noreply.github.com>
2019-06-25 13:51:50 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -10,23 +10,23 @@ body = JSON.parse(result);
 if(result.indexOf('aweme_list') != -1){
   body.aweme_list.forEach((element, index)=>{
     if(element.hasOwnProperty('raw_ad_data')){      
-    body.aweme_list.splice(index, 1);
+      body.aweme_list.splice(index, 1);
     }
   });
   body.aweme_list.forEach((element, index)=>{
     if(element.hasOwnProperty('simple_promotions')){      
-    delete body.aweme_list[index].simple_promotions;
+      delete body.aweme_list[index].simple_promotions;
     }
   });
   body.aweme_list.forEach((element, index) => {
-    if (element.interaction_stickers !== null) {
+    if (element['interaction_stickers'] !== null) {
       body.aweme_list[index].interaction_stickers = null;
     }
   });
-  body['aweme_list'].forEach((element, index) => {
-    if (element.prevent_download === true) {
-      body['aweme_list'][index].status.reviewed = 1;
-      body['aweme_list'][index].prevent_download = false;
+  body.aweme_list.forEach((element, index) => {
+    if (element['prevent_download'] === true) {
+      body.aweme_list[index].status.reviewed = 1;
+      body.aweme_list[index].prevent_download = false;
     }
   });
 }
