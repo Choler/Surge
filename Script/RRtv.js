57f8c9611ae574df6cd1590295a2cb24d3a65db0
Choler <43330069+Choler@users.noreply.github.com>
2019-06-25 17:56:57 +0800

    Add files via upload

--- Script/RRtv.js
+++ Script/RRtv.js
@@ -1,59 +1,51 @@
-const path1 = "/user/profile";
-const path2 = "/v3plus/medal/getAll";
-const path3 = "/v3plus/index/channel";
-const path4 = "/v3plus/video/getTopFeed";
-const path5 = "/v3plus/index/todayChoice";
+const list1 = {
+    "name":"1080P",
+    "endTime":"2022-01-01 00:00:00",
+    "imgUrl": "http://img.rr.tv/medal/20180607/o_1528365641950.png",
+    "id":25
+    };
+const list2 = {
+    "description": "解锁原画",
+    "icon": "jiesuoyuanhua",
+    "endTime": 1640966400000,
+    "effectObject": "video",
+    "func": "originalPainting",
+    "action": "play"
+    };
 
 let result = $response.body;
-
-if ($request.url.indexOf(path1) != -1) {
-  var obj = JSON.parse(result);
-  obj.data.user.medalList = JSON.parse(
-    '[{"name":"看剧必备","endTime":"2022-01-01 00:00:00","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]'
-  );
-  obj.data.user.privilegeList = JSON.parse(
-    '[{"effectObject":"danmu","action":"send","func":"superBarrageBlue","description":"超级弹幕","icon":"chaojidanmu","endTime":1640966400000},{"effectObject":"video","action":"play","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":1640966400000},{"effectObject":"video","action":"play","func":"noAd","description":"看剧无广告","icon":"kanjuwuguanggao","endTime":1640966400000},{"effectObject":"growth","action":"play","func":"0.4","description":"经验值加成+40%","icon":"jingyanzhijiacheng","endTime":1640966400000},{"effectObject":"video","action":"play","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]'
-  );
+if ($request.url.indexOf('profile') != -1) {
+  obj = JSON.parse(result);
+  obj.data.user.medalList.splice(0, 0, list1);
+  obj.data.user.privilegeList.splice(0, 0, list2);
   body = JSON.stringify(obj);
+  $done({body});
 }
 
-if ($request.url.indexOf(path2) != -1) {
-  var obj = JSON.parse(result);
-  obj.data.medalList.forEach((element, index) => {
-    if (element.medal.id === 1) {
+if($request.method == 'OPTIONS'){
+  $done({})
+} else {
+  if ($request.url.indexOf('medal') != -1) {
+    obj = JSON.parse(result);
+    obj.data.medalList.forEach((element, index) => {
+    if (element['medal']['id'] === 25) {
       obj.data.medalList[index].medal.endTime = "2022-01-01 00:00:00";
-    }
-  });
+      }
+    });
+  };
   body = JSON.stringify(obj);
+  $done({body});
 }
 
-if ($request.url.indexOf(path3) != -1) {
-  var obj = JSON.parse(result);
-  obj.data.sections.forEach((element, index) => {
-    if (element.sectionType === "AD") {
-      obj.data.sections.splice(index, 1);
-    }
-  });
-  body = JSON.stringify(obj);
-}
+/**********************************************************
+[说明]
+此脚本仅适用于人人视频1.0版本
+各大助手软件下载的人人旧版均有效
+如果不需要去广告就只添加第一个脚本
 
-if ($request.url.indexOf(path4) != -1) {
-  var obj = JSON.parse(result);
-  delete obj.data.notice;
-  obj.data.indexView.interestingList.splice(3, 1);
-  body = JSON.stringify(obj);
-}
-
-if ($request.url.indexOf(path5) != -1) {
-  var obj = JSON.parse(result);
-  delete obj.data.notice;
-  obj.data.sections.splice(-1, 1);
-  obj.data.sections.forEach((element, index) => {
-    if (element.sectionType === "AD") {
-      obj.data.sections.splice(index, 1);
-    }
-  });
-  body = JSON.stringify(obj);
-}
-
-$done({body});
+[Script]
+http-response ^https:\/\/api\.rr\.tv\/.*(profile|Medal) script-path=https://raw.githubusercontent.com/Choler/Surge/master/Script/RRtv.js,requires-body=true,max-size=16384
+http-response ^https:\/\/api\.rr\.tv\/.*(channel|Choice|Feed) script-path=https://raw.githubusercontent.com/Choler/Surge/master/Script/RRtv-ad.js,requires-body=true,max-size=524288
+[MITM]
+hostname = api.rr.tv
+**********************************************************/
