3793db4357a43b32914acbd639042482a32e6c6f
Choler <43330069+Choler@users.noreply.github.com>
2019-06-28 13:41:09 +0800

    Update WeChat.js

--- Script/WeChat.js
+++ Script/WeChat.js
@@ -3,12 +3,3 @@ body['advertisement_num'] = 0;
 body['advertisement_info'] = [];
 result = JSON.stringify(body);
 $done({body: result});
-
-
-/**********************************************************
-[Script]
-http-response ^https:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js,requires-body=true
-
-[MITM]
-hostname = mp.weixin.qq.com
-**********************************************************/
