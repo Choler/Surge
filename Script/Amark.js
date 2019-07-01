650029cadba8079fc5374f868350e2e68f9c2407
Choler <43330069+Choler@users.noreply.github.com>
2019-07-02 01:02:01 +0800

    Add files via upload

--- Script/Amark.js
+++ Script/Amark.js
@@ -1,12 +1,10 @@
 if ($request.url.indexOf('watermark') > 0) {
-  result = $request.url.replace(/&watermark=\d/, "")
-  $done({url: result});
+  $done({url: $request.url.replace(/&watermark=\d/, "")});
 } else {
   $done();
 }
 if ($request.url.indexOf('vide.f7') > 0) {
-  result = $request.url.replace(/vide\.f7/, "vide.f")
-  $done({url: result});
+  $done({url: $request.url.replace(/vide\.f7/, "vide.f")});
 } else {
   $done();
 }
