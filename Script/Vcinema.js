44ecc10bc9ec47fd59333773b0929661d186e04d
Choler <43330069+Choler@users.noreply.github.com>
2019-07-12 16:45:32 +0800

    Add files via upload

--- Script/Vcinema.js
+++ Script/Vcinema.js
@@ -1,11 +1,9 @@
 let body = JSON.parse($response.body);
-if ($request.url.indexOf('splash') > 0 ||$request.url.indexOf('activities') > 0) {
-  body.content = [];
-  $done({body: JSON.stringify(body)})
+if ($request.url.indexOf('user') > 0) {
+  body.content.user_vip_end_date = "2022.01.01";
+  body.content.user_vip_end_date_desc = "2022-01-01";
+  $done({body: JSON.stringify(body)});
 } else {
-  delete body.content.user_modal_list;
-  body.content.pass_days = "-38"
-  body.content.user_vip_end_date = "2019.08.19"
-  body.content.user_vip_end_date_desc = "2019-08-19"
-  $done({body: JSON.stringify(body)})
+  body.content = [];
+  $done({body: JSON.stringify(body)});
 }
