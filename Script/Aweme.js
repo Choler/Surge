e4bb5eb67778b167da6fec78bfc748e065f0a715
Choler <43330069+Choler@users.noreply.github.com>
2019-07-16 15:38:12 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -15,20 +15,32 @@ keyword3.forEach(function(k) {
   result = replaceAll(result, k, '"allow_download":true');
 });
 let body = JSON.parse(result);
-if(body.aweme_list){
-  body.aweme_list.forEach((element, index)=>{
-    if(element.hasOwnProperty('raw_ad_data')){      
+if (body.aweme_list) {
+  body.aweme_list.forEach((element, index) => {
+    if (element.hasOwnProperty('raw_ad_data')) {
       body.aweme_list.splice(index, 1);
     }
   });
-  body.aweme_list.forEach((element, index)=>{
-    if(element.hasOwnProperty('simple_promotions')){      
+  body.aweme_list.forEach((element, index) => {
+    if (element.hasOwnProperty('simple_promotions')) {
       delete body.aweme_list[index].simple_promotions;
     }
   });
   body.aweme_list.forEach((element, index) => {
     if (element['interaction_stickers'] !== null) {
       body.aweme_list[index].interaction_stickers = null;
+    }
+  });
+}
+if (body.data) {
+  body.data.forEach((element, index) => {
+    if (element.aweme.hasOwnProperty('simple_promotions')) {
+      delete body.data[index].aweme.simple_promotions;
+    }
+  });
+  body.data.forEach((element, index) => {
+    if (element.aweme['interaction_stickers'] !== null) {
+      body.data[index].aweme.interaction_stickers = null;
     }
   });
 }
