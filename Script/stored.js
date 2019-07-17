0e939c504b1753faad90bf0931c03327a60c2e36
Choler <43330069+Choler@users.noreply.github.com>
2019-07-17 18:21:40 +0800

    Add files via upload

--- Script/stored.js
+++ Script/stored.js
@@ -1,10 +1,10 @@
-const line = 'http://119.29.29.29/d?dn=xcu-jn-iosapps.ccgslb.com.cn';
+const url = 'http://119.29.29.29/d?dn=xcu-jn-iosapps.ccgslb.com.cn';
 if ($network.v4.primaryInterface === 'en0') {
-  $httpClient.get(line, function(error, response, data) {
+  $httpClient.get(url, function(error, response, data) {
     if (error) {
       $done({server: '8.8.8.8'});
     } else {
-      $done({ addresses: data.split(';'), ttl: 600 });
+      $done({addresses: data.split(';'), ttl: 600});
     }
   });
 } else {
