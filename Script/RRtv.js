6dd8eea1bd06e9d63492eb2e502211085fc89f24
Choler <43330069+Choler@users.noreply.github.com>
2019-07-18 04:49:10 +0800

    Add files via upload

--- Script/RRtv.js
+++ Script/RRtv.js
@@ -1,9 +1,15 @@
+const list = {
+  "endTime": "2022-01-01 00:00:00",
+  "id": 25,
+  "imgUrl": "http://img.rr.tv/medal/20180607/o_1528365641950.png",
+  "name": "1080P"
+};
 if ($request.method === 'OPTIONS') {
   $done({});
 } else {
   let body = JSON.parse($response.body);
   if ($request.url.indexOf('profile') > 0) {
-    body.data.user.medalList = JSON.parse('[{"name":"1080P","endTime":"2022-01-01 00:00:00","imgUrl":"http://img.rr.tv/medal/20180607/o_1528365641950.png","id":25}]');
+    body.data.user.medalList.splice(0, 0, list)
     body.data.user.privilegeList = JSON.parse('[{"id":null,"createTime":null,"updateTime":null,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]');
   }
   if ($request.url.indexOf('medal') > 0) {
