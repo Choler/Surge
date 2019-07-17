e2faf9c4961562155a4c012a6bab4b376c8a96b2
Choler <43330069+Choler@users.noreply.github.com>
2019-07-18 04:38:23 +0800

    Add files via upload

--- Script/RRtv.js
+++ Script/RRtv.js
@@ -1,38 +1,16 @@
-const list1 = {
-    "name":"1080P",
-    "endTime":"2022-01-01 00:00:00",
-    "imgUrl": "http://img.rr.tv/medal/20180607/o_1528365641950.png",
-    "id":25
-    };
-const list2 = {
-    "description": "解锁原画",
-    "icon": "jiesuoyuanhua",
-    "endTime": 1640966400000,
-    "effectObject": "video",
-    "func": "originalPainting",
-    "action": "play"
-    };
-
-let result = $response.body;
-if ($request.url.indexOf('profile') != -1) {
-  obj = JSON.parse(result);
-  obj.data.user.medalList.splice(0, 0, list1);
-  obj.data.user.privilegeList.splice(0, 0, list2);
-  body = JSON.stringify(obj);
-  $done({body});
+if ($request.method === 'OPTIONS') {
+  $done({});
 }
-
-if($request.method == 'OPTIONS'){
-  $done({})
-} else {
-  if ($request.url.indexOf('medal') != -1) {
-    obj = JSON.parse(result);
-    obj.data.medalList.forEach((element, index) => {
-    if (element['medal']['id'] === 25) {
-      obj.data.medalList[index].medal.endTime = "2022-01-01 00:00:00";
-      }
-    });
-  };
-  body = JSON.stringify(obj);
-  $done({body});
+let body = JSON.parse($response.body);
+if ($request.url.indexOf('profile') > 0) {
+  body.data.user.medalList = JSON.parse('[{"name":"1080P","endTime":"2022-01-01 00:00:00","imgUrl":"http://img.rr.tv/medal/20180607/o_1528365641950.png","id":25}]');
+  body.data.user.privilegeList = JSON.parse('[{"id":null,"createTime":null,"updateTime":null,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]');
 }
+if ($request.url.indexOf('medal') > 0) {
+  body.data.medalList.forEach((element, index) => {
+  if (element['medal']['id'] === 25) {
+    body.data.medalList[index].medal.endTime = "2022-01-01 00:00:00";
+    }
+  });
+}
+$done({body: JSON.stringify(body)});
