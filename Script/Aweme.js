9072d76a1b0cc544cd6c703951f0215010aed101
Choler <43330069+Choler@users.noreply.github.com>
2019-07-19 14:10:36 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,46 +1,29 @@
-function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(find, 'g'), replace);
-}
-var result = $response.body;
-var keyword1 = ['watermark=1'];
-var keyword2 = ['"reviewed":0'];
-var keyword3 = ['"allow_download":false'];
-keyword1.forEach(function(k) {
-  result = replaceAll(result, k, 'watermark=0');
-});
-keyword2.forEach(function(k) {
-  result = replaceAll(result, k, '"reviewed":1');
-});
-keyword3.forEach(function(k) {
-  result = replaceAll(result, k, '"allow_download":true');
-});
+var result = $response.body.replace(/watermark=1/g, "watermark=0");
 let body = JSON.parse(result);
 if (body.aweme_list) {
+  body.aweme_list.forEach((item) => {
+    item.status.reviewed = 1;
+    item.interaction_stickers = null;
+    item.video_control.allow_download = true;
+  });
   body.aweme_list.forEach((element, index) => {
     if (element.hasOwnProperty('raw_ad_data')) {
       body.aweme_list.splice(index, 1);
     }
-  });
-  body.aweme_list.forEach((element, index) => {
     if (element.hasOwnProperty('simple_promotions')) {
       delete body.aweme_list[index].simple_promotions;
     }
   });
-  body.aweme_list.forEach((element, index) => {
-    if (element['interaction_stickers'] !== null) {
-      body.aweme_list[index].interaction_stickers = null;
-    }
-  });
 }
 if (body.data) {
+  body.data.forEach((item) => {
+    item.aweme.status.reviewed = 1;
+    item.aweme.interaction_stickers = null;
+    item.aweme.video_control.allow_download = true;
+  });
   body.data.forEach((element, index) => {
     if (element.aweme.hasOwnProperty('simple_promotions')) {
       delete body.data[index].aweme.simple_promotions;
-    }
-  });
-  body.data.forEach((element, index) => {
-    if (element.aweme['interaction_stickers'] !== null) {
-      body.data[index].aweme.interaction_stickers = null;
     }
   });
 }
