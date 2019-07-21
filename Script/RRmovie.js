10590a88f9380a55896b2e316ea1b21a0c849ff9
Choler <43330069+Choler@users.noreply.github.com>
2019-07-21 10:12:55 +0800

    Add files via upload

---
+++ Script/RRmovie.js
@@ -0,0 +1,3 @@
+let headers = $request.headers;
+headers['st'] = '109de4db0b5fd6190d5de1655098c711';
+$done({headers});
--- Script/RRtv.js
+++ Script/RRtv.js
@@ -1,16 +1,17 @@
-const list = {
-  "endTime": "2022-01-01 00:00:00",
-  "id": 25,
-  "imgUrl": "http://img.rr.tv/medal/20180607/o_1528365641950.png",
-  "name": "1080P"
-};
+const list = {"endTime": "2022-01-01 00:00:00","id": 25,"imgUrl": "http://img.rr.tv/medal/20180607/o_1528365641950.png","name": "1080P"}
+const list1 = [{"id":1,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]
+const list2 = [{"id":null,"createTime":null,"updateTime":null,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]
+
 if ($request.method === 'OPTIONS') {
   $done({});
 } else {
   let body = JSON.parse($response.body);
   if ($request.url.indexOf('profile') > 0) {
     body.data.user.medalList.splice(0, 0, list)
-    body.data.user.privilegeList = JSON.parse('[{"id":null,"createTime":null,"updateTime":null,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1640966400000}]');
+    body.data.user.privilegeList = list2;
+  }
+  if ($request.url.indexOf('privilege') > 0) {
+    body.data = list1;
   }
   if ($request.url.indexOf('medal') > 0) {
     body.data.medalList.forEach((element, index) => {
