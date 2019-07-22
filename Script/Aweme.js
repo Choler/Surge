ab56f9b4b67086ad081239e323a31c9606425e91
Choler <43330069+Choler@users.noreply.github.com>
2019-07-22 13:22:09 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -2,32 +2,24 @@ var result = $response.body.replace(/watermark=1/g, "watermark=0");
 let body = JSON.parse(result);
 if (body.aweme_list) {
   body.aweme_list.forEach((item) => {
+    delete item.poi_info;
+    delete item.sticker_detail;
+    delete item.simple_promotions;
     item.status.reviewed = 1;
     item.interaction_stickers = null;
     item.video_control.allow_download = true;
-  });
-  body.aweme_list.forEach((element, index) => {
-    if (element.hasOwnProperty('raw_ad_data')) {
-      body.aweme_list.splice(index, 1);
-    }
-    if (element.hasOwnProperty('simple_promotions')) {
-      delete body.aweme_list[index].simple_promotions;
-    }
-    if (element.hasOwnProperty('poi_info')) {
-      delete body.aweme_list[index].poi_info;
-    }
+    if (item.raw_ad_data) {
+      body.aweme_list.splice(body.aweme_list.findIndex(item => item.is_ads === true), 1)
+      $notification.post("Delete ADS", "", "")
+    };
   });
 }
 if (body.data) {
   body.data.forEach((item) => {
+    delete item.aweme.simple_promotions;
     item.aweme.status.reviewed = 1;
     item.aweme.interaction_stickers = null;
     item.aweme.video_control.allow_download = true;
-  });
-  body.data.forEach((element, index) => {
-    if (element.aweme.hasOwnProperty('simple_promotions')) {
-      delete body.data[index].aweme.simple_promotions;
-    }
   });
 }
 $done({body: JSON.stringify(body)});
