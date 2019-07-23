a347ba6932383a87a2f294a1da3cf341f8b65c83
Choler <43330069+Choler@users.noreply.github.com>
2019-07-23 20:18:45 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -4,41 +4,34 @@ if (body.aweme_list) {
   body.aweme_list.forEach((element, index) => {
     if (element.poi_info) {
       delete body.aweme_list[index].poi_info;
-    };
+    }
     if (element.sticker_detail) {
       delete body.aweme_list[index].sticker_detail;
-    };
+    }
     if (element.simple_promotions) {
       delete body.aweme_list[index].simple_promotions;
-    };
+    }
     if (element.is_ads !== false) {
-      body.aweme_list.splice(index, 1)
-    };
-    if (element.status.reviewed !== 1) {
-      body.aweme_list[index].status.reviewed = 1;
-    };
+      body.aweme_list.splice(index, 1);
+    }
     if (element.interaction_stickers !== null) {
       body.aweme_list[index].interaction_stickers = null;
-    };
+    }
     if (element.video_control.allow_download !== true) {
+      body.aweme_list[index].status.reviewed = 1;
       body.aweme_list[index].video_control.allow_download = true;
-    };
+    }
   });
 }
 if (body.data) {
   body.data.forEach((element, index) => {
     if (element.aweme.simple_promotions) {
       delete body.data[index].aweme.simple_promotions;
-    };
-    if (element.aweme.status.reviewed !== 1) {
-      body.data[index].aweme.status.reviewed = 1;
-    };
-    if (element.aweme.interaction_stickers !== null) {
-      body.data[index].aweme.interaction_stickers = null;
-    };
+    }
     if (element.aweme.video_control.allow_download !== true) {
+      body.data[index].aweme.status.reviewed = 1;
       body.data[index].aweme.video_control.allow_download = true;
     }
   });
 }
-$done({body: JSON.stringify(body)});
+$done({ body: JSON.stringify(body) });
