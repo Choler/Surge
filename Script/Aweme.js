0923b4747009cbf1164380bd87119f7f8b90b16d
Choler <43330069+Choler@users.noreply.github.com>
2019-07-23 09:25:33 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,29 +1,41 @@
 var result = $response.body.replace(/watermark=1/g, "watermark=0");
 let body = JSON.parse(result);
 if (body.aweme_list) {
-  body.aweme_list.forEach(item => {
-    delete item.poi_info;
-    delete item.sticker_detail;
-    delete item.simple_promotions;
-    item.status.reviewed = 1;
-    item.interaction_stickers = null;
-    item.video_control.allow_download = true;
-    if (item.is_ads === true) {
-      body.aweme_list.splice(body.aweme_list.findIndex(item => item.raw_ad_data), 1);
-    }
+  body.aweme_list.forEach((element, index) => {
+    if (element.poi_info) {
+      delete body.aweme_list[index].poi_info;
+    };
+    if (element.sticker_detail) {
+      delete body.aweme_list[index].sticker_detail;
+    };
+    if (element.simple_promotions) {
+      delete body.aweme_list[index].simple_promotions;
+    };
+    if (element.is_ads !== false) {
+      body.aweme_list.splice(index, 1)
+    };
+    if (element.status.reviewed !== 1) {
+      body.aweme_list[index].status.reviewed = 1;
+    };
+    if (element.interaction_stickers !== null) {
+      body.aweme_list[index].interaction_stickers = null;
+    };
+    if (element.video_control.allow_download !== true) {
+      body.aweme_list[index].video_control.allow_download = true;
+    };
   });
 }
 if (body.data) {
   body.data.forEach((element, index) => {
     if (element.aweme.simple_promotions) {
       delete body.data[index].aweme.simple_promotions;
-    }
+    };
     if (element.aweme.status.reviewed !== 1) {
       body.data[index].aweme.status.reviewed = 1;
-    }
-    if (element.aweme.status.reviewed !== null) {
+    };
+    if (element.aweme.interaction_stickers !== null) {
       body.data[index].aweme.interaction_stickers = null;
-    }
+    };
     if (element.aweme.video_control.allow_download !== true) {
       body.data[index].aweme.video_control.allow_download = true;
     }
