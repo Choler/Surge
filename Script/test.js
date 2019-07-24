d5b47cf9ff2a46e360bd34609e70a8a5f2203c0e
Choler <43330069+Choler@users.noreply.github.com>
2019-07-24 16:42:27 +0800

    Add files via upload

--- Script/test.js
+++ Script/test.js
@@ -4,13 +4,22 @@ result = result.replace(/"reviewed":0/g, '"reviewed":1');
 result = result.replace(/"allow_download":false/g, '"allow_download":true');
 let body = JSON.parse(result);
 if (body.aweme_list) {
-  body.aweme_list.forEach((item) => {
-    delete item.poi_info;
-    delete item.sticker_detail;
-    delete item.simple_promotions;
-    if (item.raw_ad_data) {
-      body.aweme_list.splice(body.aweme_list.findIndex(item => item.is_ads === true), 1)
-    };
+  body.aweme_list.forEach((element, index) => {
+    if (element.poi_info) {
+      delete body.aweme_list[index].poi_info;
+    }
+    if (element.sticker_detail) {
+      delete body.aweme_list[index].sticker_detail;
+    }
+    if (element.simple_promotions) {
+      delete body.aweme_list[index].simple_promotions;
+    }
+    if (element.is_ads !== false) {
+      body.aweme_list.splice(index, 1);
+    }
+    if (element.interaction_stickers !== null) {
+      body.aweme_list[index].interaction_stickers = null;
+    }
   });
 }
 $done({body: JSON.stringify(body)});
