b4e391d3e20bc0487117a26b1d4244250527de7b
Choler <43330069+Choler@users.noreply.github.com>
2019-07-30 19:40:36 +0800

    Add files via upload

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,37 +1,45 @@
-var result = $response.body.replace(/watermark=1/g, "watermark=0");
-let body = JSON.parse(result);
-if (body.aweme_list) {
-  body.aweme_list.forEach((element, index) => {
-    if (element.poi_info) {
-      delete body.aweme_list[index].poi_info;
+let body = $response.body.replace(/watermark=1/g, "watermark=0")
+var obj = JSON.parse(body)
+if ($request.url.indexOf('life') > 0) {
+  $done({})
+} else if(obj.aweme_list){
+  for(var i = obj.aweme_list.length -1; i >= 0 ; i--){
+    if(obj.aweme_list[i].is_ads != false){
+      obj.aweme_list.splice(i,1)
     }
-    if (element.sticker_detail) {
-      delete body.aweme_list[index].sticker_detail;
+    if(obj.aweme_list[i].poi_info){
+      delete obj.aweme_list[i].poi_info
     }
-    if (element.simple_promotions) {
-      delete body.aweme_list[index].simple_promotions;
+    if(obj.aweme_list[i].sticker_detail){
+      delete obj.aweme_list[i].sticker_detail
     }
-    if (element.is_ads !== false) {
-      body.aweme_list.splice(index, 1);
+    if(obj.aweme_list[i].simple_promotions){
+      delete obj.aweme_list[i].simple_promotions
     }
-    if (element.interaction_stickers !== null) {
-      body.aweme_list[index].interaction_stickers = null;
+    if(obj.aweme_list[i].interaction_stickers != null){
+      obj.aweme_list[i].interaction_stickers = null
     }
-    if (element.video_control.allow_download !== true) {
-      body.aweme_list[index].status.reviewed = 1;
-      body.aweme_list[index].video_control.allow_download = true;
+    if(obj.aweme_list[i].video_control.allow_download != true){
+      obj.aweme_list[i].status.reviewed = 1
+      obj.aweme_list[i].video_control.allow_download = true
     }
-  });
+  }
+  $done({body: JSON.stringify(obj)})
+} else if(obj.data){
+  for(var i = obj.data.length -1; i >= 0 ; i--){
+    if (obj.data[i].aweme){
+      if(obj.data[i].aweme.simple_promotions){
+        delete obj.data[i].aweme.simple_promotions
+      }
+      if(obj.data[i].aweme.video_control.allow_download != true){
+        obj.data[i].aweme.status.reviewed = 1
+        obj.data[i].aweme.video_control.allow_download = true
+      }
+    } else {
+      obj.data.splice(i,1)
+    }
+  }
+  $done({body: JSON.stringify(obj)})
+} else {
+  $done({})
 }
-if (body.data) {
-  body.data.forEach((element, index) => {
-    if (element.aweme.simple_promotions) {
-      delete body.data[index].aweme.simple_promotions;
-    }
-    if (element.aweme.video_control.allow_download !== true) {
-      body.data[index].aweme.status.reviewed = 1;
-      body.data[index].aweme.video_control.allow_download = true;
-    }
-  });
-}
-$done({ body: JSON.stringify(body) });
