4419ae4114da256787eeab8a927378c88ede81c9
Choler <43330069+Choler@users.noreply.github.com>
2019-08-10 13:11:35 +0800

    Add files via upload

---
+++ Script/RRad.js
@@ -0,0 +1,13 @@
+if ($request.method === "OPTIONS") {
+  $done({})
+} else {
+  var obj = JSON.parse($response.body)
+  if (obj.data.sections) {
+    for (var i = obj.data.sections.length -1; i >= 0 ; i--) {
+      if(obj.data.sections[i].sectionType === "AD"){
+        obj.data.sections.splice(i,1)
+      }
+    }
+  }
+  $done({body: JSON.stringify(obj)})
+}
