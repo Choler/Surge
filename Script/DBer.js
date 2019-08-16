5eac520f45e9cd9ea64acc6a2117fdaad0399a29
Choler <43330069+Choler@users.noreply.github.com>
2019-08-17 00:30:13 +0800

    Add files via upload

--- Script/DBer.js
+++ Script/DBer.js
@@ -1,19 +1,19 @@
-$httpClient.get('https://api.lynne.ink/release/?v=0.1.1&m=s', function(error, response, data){
+$httpClient.get('https://api.lynne.ink/release/?v=0.2&m=s', function(error, response, data) {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-    var obj = JSON.parse(data).detail
-    var cache = $persistentStore.read()
-    for(var i = obj.iOS.length -1; i >= 0 ; i--) {
-      if(obj.iOS[i].betaVersion != false){
-        if(cache == null){
-          $persistentStore.write(obj.iOS[i].buildID)
-        } else if(obj.iOS[i].buildID != cache && cache != null){
-          $persistentStore.write(obj.iOS[i].buildID)
-          $notification.post("系统更新", "iOS " + obj.iOS[i].version + " Developer Beta " + obj.iOS[i].betaVersion + " 已由Apple发布！", "")
+    var obj = JSON.parse(data).detail;
+    var cache = $persistentStore.read();
+    for (var i = obj.iOS.length -1; i >= 0 ; i--) {
+      if (obj.iOS[i].betaVersion != false) {
+        if (cache == null) {
+          $persistentStore.write(obj.iOS[i].buildID);
+        } else if (obj.iOS[i].buildID != cache && cache != null) {
+          $persistentStore.write(obj.iOS[i].buildID);
+          $notification.post("系统更新  (" + obj.iOS[i].buildID + ")", "iOS " + obj.iOS[i].version + " Developer Beta " + obj.iOS[i].betaVersion + " 已由Apple发布！", "");
         }
       }
     }
   }
 })
-$done()
+$done();
