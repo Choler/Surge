b4f2abe51948a6e9efd39d26526c1a6ff390ff79
Choler <43330069+Choler@users.noreply.github.com>
2019-08-16 10:57:06 +0800

    Add files via upload

---
+++ Script/DBer.js
@@ -0,0 +1,20 @@
+$httpClient.get('https://api.lynne.ink/release/?v=0.1.1&m=s', function(error, response, data){
+  if (error) {
+    console.log(error)
+  } else {
+    var obj = JSON.parse(data).detail
+    var cache = $persistentStore.read()
+    console.log(cache)
+    for(var i = obj.iOS.length -1; i >= 0 ; i--) {
+      if(obj.iOS[i].betaVersion != false){
+        if(cache == null){
+          $persistentStore.write(obj.iOS[i].buildID)
+        } else if(obj.iOS[i].buildID != cache && cache != null){
+          $persistentStore.write(obj.iOS[i].buildID)
+          $notification.post("系统更新", "iOS " + obj.iOS[i].version + " Developer Beta " + obj.iOS[i].betaVersion + " 已由Apple发布！", "")
+        }
+      }
+    }
+  }
+})
+$done()
