f8f2710e4338c5129e02d09cf3e3444dfe3bd582
Choler <43330069+Choler@users.noreply.github.com>
2019-08-25 12:12:56 +0800

    同步更新

--- Script/TikTok.js
+++
@@ -1,8 +0,0 @@
-var obj = JSON.parse($response.body);
-if (obj.aweme_list) {
-  for (var i = obj.aweme_list.length -1; i >= 0 ; i--) {
-    var arr = obj.aweme_list[i].video;
-    arr.download_addr = arr.play_addr;
-  }
-}
-$done({body: JSON.stringify(obj)});
