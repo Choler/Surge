61959c18d4820277fe9dd3431676d2444496d0ad
Choler <43330069+Choler@users.noreply.github.com>
2019-09-10 10:32:35 +0800

    同步更新

--- Script/failed.js
+++ Script/failed.js
@@ -6,4 +6,7 @@ SCRIPT,falied,PROXY,requires-resolve
 rule falied script-path=https://Choler.github.io/Surge/Script/failed.js
 */
 
-$done({matched: ($request.dnsResult.v4Addresses[0] === "127.0.0.1")});
+var ip_1 = $request.dnsResult.v4Addresses[0] === "0.0.0.0";
+var ip_2 = $request.dnsResult.v4Addresses[0] === "127.0.0.1";
+
+$done({matched: (ip_1 || ip_1)});
