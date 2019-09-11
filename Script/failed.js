2f8c78caae03a266164a3fb47ae1fed41d514b19
Choler <43330069+Choler@users.noreply.github.com>
2019-09-11 12:20:14 +0800

    同步更新

--- Script/failed.js
+++ Script/failed.js
@@ -9,4 +9,4 @@ rule falied script-path=https://Choler.github.io/Surge/Script/failed.js
 var ip_1 = $request.dnsResult.v4Addresses[0] === "0.0.0.0";
 var ip_2 = $request.dnsResult.v4Addresses[0] === "127.0.0.1";
 
-$done({matched: (ip_1 || ip_1)});
+$done({matched: (ip_1 || ip_2)});
