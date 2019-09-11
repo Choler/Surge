ce30e57b4b960bca49d3057cc01a9753b5c861e4
Choler <43330069+Choler@users.noreply.github.com>
2019-09-11 15:43:26 +0800

    问题修正

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -9,6 +9,16 @@ http-response ^https://[\s\S]*\/aweme/v1/(feed|aweme/post|follow/feed)/ requires
 hostname = *.amemv.com, *.snssdk.com
 */
 
+let arr = {
+  "allow_download": true,
+  "share_type": 0,
+  "show_progress_bar": 0,
+  "draft_progress_bar": 0,
+  "allow_duet": true,
+  "allow_react": true,
+  "prevent_download_type": 2,
+  "allow_dynamic_wallpaper": false
+};
 let body = $response.body.replace(/watermark=1/g, "watermark=0");
 var obj = JSON.parse(body);
 if (obj.aweme_list) {
@@ -25,13 +35,8 @@ if (obj.aweme_list) {
     if (obj.aweme_list[i].simple_promotions) {
       delete obj.aweme_list[i].simple_promotions;
     }
-    if (obj.aweme_list[i].interaction_stickers != null) {
-      obj.aweme_list[i].interaction_stickers = null;
-    }
-    if (obj.aweme_list[i].video_control.allow_download != true) {
-      obj.aweme_list[i].status.reviewed = 1;
-      obj.aweme_list[i].video_control.allow_download = true;
-    }
+    obj.aweme_list[i].status.reviewed = 1;
+    obj.aweme_list[i].video_control = arr;
   }
   $done({body: JSON.stringify(obj)});
 } else if (obj.data) {
@@ -43,10 +48,8 @@ if (obj.aweme_list) {
       if (obj.data[i].aweme.simple_promotions) {
         delete obj.data[i].aweme.simple_promotions;
       }
-      if (obj.data[i].aweme.video_control.allow_download != true) {
-        obj.data[i].aweme.status.reviewed = 1;
-        obj.data[i].aweme.video_control.allow_download = true;
-      }
+      obj.data[i].aweme.status.reviewed = 1;
+      obj.data[i].aweme.video_control = arr;
     } else {
       obj.data.splice(i, 1);
     }
