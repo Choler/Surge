43ecb8e9624d37bd715bdefb3fff93881973e42f
Choler <43330069+Choler@users.noreply.github.com>
2019-11-25 22:16:56 +0800

    问题修正

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -23,7 +23,7 @@ let body = $response.body.replace(/watermark=1/g, "watermark=0");
 var obj = JSON.parse(body);
 if (obj.aweme_list) {
   for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
-    if (obj.aweme_list[i].raw_ad_data) {
+    if (obj.aweme_list[i].is_ads == true) {
       obj.aweme_list.splice(i, 1);
     }
     if (obj.aweme_list[i].poi_info) {
