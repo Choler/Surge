381a1f0a10848e8d5a74b9585f31e4c84486020e
Choler <43330069+Choler@users.noreply.github.com>
2020-02-16 22:53:37 +0800

    问题修正

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -1,7 +1,8 @@
 /*
 [Script]
-http-request ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) script-path=https://Choler.github.io/Surge/Script/Aweme.jsAweme.js
-http-response ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) requires-body=true,script-path=https://Choler.github.io/Surge/Script/Aweme.jsAweme.js
+http-request ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) script-path=https://Choler.github.io/Surge/Script/Aweme.js
+http-response ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) requires-body=true,script-path=https://Choler.github.io/Surge/Script/Aweme.js
+
 [MITM]
 hostname = *.amemv.com
 */
