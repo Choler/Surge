aaa12fcf247252416b6711d31cc393bb0f9db73c
Choler <43330069+Choler@users.noreply.github.com>
2020-02-19 14:30:44 +0800

    开启动态壁纸下载

--- Script/Aweme.js
+++ Script/Aweme.js
@@ -18,26 +18,24 @@ function request() {
 }
 
 function response() {
+  var re = /\u0026watermark=1/g;
   var obj = JSON.parse($response.body);
   if (obj.aweme_list) {
     for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
+      obj.aweme_list[i].status.reviewed = 1;
+      obj.aweme_list[i].video_control.allow_download = true;
+      obj.aweme_list[i].video_control.allow_dynamic_wallpaper = true;
       if (obj.aweme_list[i].is_ads != false) {
         obj.aweme_list.splice(i, 1);
-      }
-      if (obj.aweme_list[i].status.reviewed != 1) {
-        obj.aweme_list[i].status.reviewed = 1;
-        obj.aweme_list[i].video_control.allow_download = true;
       }
     }
   }
   if (obj.data) {
     for (var i = obj.data.length - 1; i >= 0; i--) {
-      if (obj.data[i].aweme.status.reviewed != 1) {
         obj.data[i].aweme.status.reviewed = 1;
         obj.data[i].aweme.video_control.allow_download = true;
-      }
+        obj.data[i].aweme.video_control.allow_dynamic_wallpaper = true;
     }
   }
-  body = JSON.stringify(obj).replace(/\u0026watermark=1/g, "");
-  $done({ body });
+  $done({ body: JSON.stringify(obj).replace(re, "") });
 }
