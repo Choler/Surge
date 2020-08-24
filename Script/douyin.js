73cf5fa2b3508c7a658e416de6f6033247b77d49
Choler <43330069+Choler@users.noreply.github.com>
2020-08-24 12:09:36 +0800

    Add files via upload

--- Script/douyin.js
+++ Script/douyin.js
@@ -1,26 +1,30 @@
-const path1 = "/v1/feed/";
-const path2 = "/v1/aweme/post/";
-const path3 = "/v1/follow/feed/";
-const path4 = "/v1/nearby/feed/";
-const path5 = "/v1/search/item/";
-const path6 = "/v1/general/search/";
-const path7 = "/v1/hot/search/video/";
+const path1 = "/v1/feed/"; // 推荐
+const path2 = "/v1/aweme/post/"; //作品
+const path3 = "/v1/follow/feed/"; // 关注
+const path4 = "/v1/nearby/feed/"; // 同城
+const path5 = "/v1/search/item/"; // 视频
+const path6 = "/v1/general/search/"; // 综合
+const path7 = "/v1/hot/search/video/"; // 热搜
 
-if ($request.url.indexOf(path1) != -1) {
-  feed();
-} else if ($request.url.indexOf(path2) != -1) {
-  post();
-} else if ($request.url.indexOf(path3) != -1) {
-  follow();
-} else if ($request.url.indexOf(path4) != -1) {
-  nearby();
-} else if ($request.url.indexOf(path5) != -1) {
-  item();
-} else if ($request.url.indexOf(path6) != -1) {
-  search();
-} else if ($request.url.indexOf(path7) != -1) {
-  hot();
-} else {
+try {
+  if ($request.url.indexOf(path1) != -1) {
+    feed();
+  } else if ($request.url.indexOf(path2) != -1) {
+    post();
+  } else if ($request.url.indexOf(path3) != -1) {
+    follow();
+  } else if ($request.url.indexOf(path4) != -1) {
+    nearby();
+  } else if ($request.url.indexOf(path5) != -1) {
+    item();
+  } else if ($request.url.indexOf(path6) != -1) {
+    search();
+  } else if ($request.url.indexOf(path7) != -1) {
+    hot();
+  } else {
+    $done({});
+  }
+} catch {
   $done({});
 }
 
@@ -31,24 +35,14 @@ function feed() {
     if (arr[i].is_ads != false) {
       arr.splice(i, 1);
     }
-    if (arr[i].status.reviewed != 1) {
-      arr[i].status.reviewed = 1;
-      arr[i].video_control.allow_download = true;
-    }
-    if (arr[i].anchor_info) {
-      arr[i].anchor_info = {};
-    }
-    if (arr[i].video.download_addr) {
-      let play = arr[i].video.play_addr.url_list;
-      arr[i].video.download_addr.url_list = play;
-    }
-    if (arr[i].video.download_suffix_logo_addr) {
-      let download = arr[i].video.download_addr;
-      arr[i].video.download_suffix_logo_addr = download;
-    }
-    if (arr[i].video.misc_download_addrs) {
-      arr[i].video.misc_download_addrs = {};
-    }
+    let play = arr[i].video.play_addr.url_list;
+    arr[i].video.download_addr.url_list = play;
+    let download = arr[i].video.download_addr;
+    arr[i].video.download_suffix_logo_addr = download;
+    arr[i].status.reviewed = 1;
+    arr[i].video_control.allow_download = true;
+    arr[i].author.room_id = 0;
+    arr[i].video.misc_download_addrs = {};
   }
   $done({ body: JSON.stringify(obj) });
 }
@@ -58,18 +52,12 @@ function post() {
   let arr = obj.aweme_list;
   if (arr != null) {
     for (var i = arr.length - 1; i >= 0; i--) {
-      if (arr[i].status.reviewed != 1) {
-        arr[i].status.reviewed = 1;
-        arr[i].video_control.allow_download = true;
-      }
-      if (arr[i].video.download_addr) {
-        let play = arr[i].video.play_addr.url_list;
-        arr[i].video.download_addr.url_list = play;
-      }
-      if (arr[i].video.download_suffix_logo_addr) {
-        let download = arr[i].video.download_addr;
-        arr[i].video.download_suffix_logo_addr = download;
-      }
+      arr[i].status.reviewed = 1;
+      arr[i].video_control.allow_download = true;
+      let play = arr[i].video.play_addr.url_list;
+      arr[i].video.download_addr.url_list = play;
+      let download = arr[i].video.download_addr;
+      arr[i].video.download_suffix_logo_addr = download;
     }
   }
   $done({ body: JSON.stringify(obj) });
@@ -79,21 +67,12 @@ function follow() {
   let obj = JSON.parse($response.body);
   let arr = obj.data;
   for (var i = arr.length - 1; i >= 0; i--) {
-    if (arr[i].aweme.status.reviewed != 1) {
-      arr[i].aweme.status.reviewed = 1;
-      arr[i].aweme.video_control.allow_download = true;
-    }
-    if (arr[i].aweme.anchor_info) {
-      arr[i].aweme.anchor_info = {};
-    }
-    if (arr[i].aweme.video.download_addr) {
-      let play = arr[i].aweme.video.play_addr.url_list;
-      arr[i].aweme.video.download_addr.url_list = play;
-    }
-    if (arr[i].aweme.video.download_suffix_logo_addr) {
-      let download = arr[i].aweme.video.download_addr;
-      arr[i].aweme.video.download_suffix_logo_addr = download;
-    }
+    arr[i].aweme.status.reviewed = 1;
+    arr[i].aweme.video_control.allow_download = true;
+    let play = arr[i].aweme.video.play_addr.url_list;
+    arr[i].aweme.video.download_addr.url_list = play;
+    let download = arr[i].aweme.video.download_addr;
+    arr[i].aweme.video.download_suffix_logo_addr = download;
   }
   $done({ body: JSON.stringify(obj) });
 }
@@ -101,7 +80,7 @@ function follow() {
 function nearby() {
   let obj = JSON.parse($response.body);
   if (obj.aweme_list) {
-      for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
+    for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
       if (obj.aweme_list[i].video) {
         if (obj.aweme_list[i].status.reviewed != 1) {
           obj.aweme_list[i].status.reviewed = 1;
@@ -115,6 +94,8 @@ function nearby() {
           let download = obj.aweme_list[i].video.download_addr;
           obj.aweme_list[i].video.download_suffix_logo_addr = download;
         }
+      } else {
+        obj.aweme_list.splice(i, 1);
       }
     }
   }
@@ -124,7 +105,7 @@ function nearby() {
 function item() {
   let obj = JSON.parse($response.body);
   if (obj.aweme_list) {
-      for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
+    for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
       if (obj.aweme_list[i].video) {
         if (obj.aweme_list[i].status.reviewed != 1) {
           obj.aweme_list[i].status.reviewed = 1;
@@ -148,17 +129,18 @@ function search() {
   let obj = JSON.parse($response.body);
   let arr = obj.data;
   for (var i = arr.length - 1; i >= 0; i--) {
-    if (arr[i].aweme_info) {
-      if (arr[i].aweme_info.video.download_addr) {
+    if (arr[i].type == 1) {
+      if (arr[i].aweme_info.is_ads) {
+        arr.splice(i, 1);
+      }
+      if (arr[i].aweme_info.video) {
         let play = arr[i].aweme_info.video.play_addr.url_list;
         arr[i].aweme_info.video.download_addr.url_list = play;
-      }
-      if (arr[i].aweme_info.video.download_suffix_logo_addr) {
         let download = arr[i].aweme_info.video.download_addr;
         arr[i].aweme_info.video.download_suffix_logo_addr = download;
+        arr[i].aweme_info.status.reviewed = 1;
+        arr[i].aweme_info.video_control.allow_download = true;
       }
-    } else {
-      arr.splice(i, 1);
     }
   }
   $done({ body: JSON.stringify(obj) });
