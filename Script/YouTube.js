c1afd39cfb4b7ace8cede4a9a23eb4c54cf373bc
Choler <43330069+Choler@users.noreply.github.com>
2020-11-25 16:58:58 +0800

    Update YouTube.js

--- Script/YouTube.js
+++ Script/YouTube.js
@@ -1,15 +1,8 @@
-/*
-[Script]
-http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https://Choler.github.io/Surge/Script/YouTube.js
-
-[MITM]
-hostname = *.googlevideo.com
-*/
-
-var data = {
-  body: "{}",
-  headers: {
-    "Content-Type": "multipart/byteranges"
-  }
-};
-$done({response: data});
+if ($request.url.indexOf("&oad") != -1) {
+  $done({ response: {body: ""} });
+} else if ($request.url.indexOf("&ctier") != -1) {
+  let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
+  $done({ response: { status: 302, headers: { Location: url } } });
+} else {
+  $done({})
+}
